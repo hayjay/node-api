d209469dbd239ee4c53d79c09e04058a6849d8a1
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2018-10-23 12:12:32 +0100

    implemented models and controller

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -4,7 +4,6 @@
     // *create a task
     // *update a task
     // *delete a task
-
 //Each of these functions uses different mongoose methods such as
     // find,
     // findById,
@@ -15,4 +14,46 @@
 
 var mongoose = require('mongoose');
     Task = mongoose.model('Tasks');
-    
+
+//function for getting all available tasks
+exports.list_all_tasks = function(req, res){
+    Task.find({}, function(err, task){
+        if(err)
+            res.send(err);
+        res.json(task);
+    });
+};
+
+exports.create_a_task = function(req, res){
+    var new_task = new Task(req.body);
+    new_task.save(function(err, task){ //call the save method on the instance of the model in a callback way
+        if(err)
+            res.send(err);
+        res.json(task);
+    });
+}
+
+
+exports.read_a_task = function(req, res) {
+    Task.findById(req.params.taskId, function(err, task) {
+        if (err)
+        res.send(err);
+        res.json(task);
+    });
+};
+
+exports.update_a_task = function(req, res) {
+    Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
+      if (err)
+        res.send(err);
+      res.json(task);
+    });
+};
+
+exports.delete_a_task = function(req,res){
+    Task.remove({_id : req.params.taskId}, function(err, res){
+        if(err)
+            res.send(err);
+        res.json({message : 'Task successfully deleted'});
+    });
+};
--- api/routes/todoListRoute.js
+++ api/routes/todoListRoute.js
@@ -6,7 +6,7 @@ module.exports = function(app){
     //todoList Routes
     app.route('tasks')
         //reuising the tasks route for get and post http method
-        .get(todoList.list_all_tasks)
+        .get(todoList.list_all_tasks)  //calling function list_all_tasks in the controller
         .post(todoList.create_a_task);
 
     app.route('/tasks/:taskId')
