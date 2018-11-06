e3c84bb4ef3b77bd2b3677bb50e3f24761fd8ad0
Ajayi Nurudeen <ajayinurudeen998@gmail.com>
2018-11-06 11:00:03 +0100

    documented authentication code

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -36,4 +36,24 @@ router.post('/register', function(req, res) {
   });
   //after creating the user, we can now easily create a token for him/her
 
+  //a piece of code to get the user id based on the token
+  //    we got back from the register endpoint.
+  //   endpoint 
+  router.get('/me', function(req, res) {
+    var token = req.headers['x-access-token'];
+    //If there is no token provided with the request the server
+    // sends back an error. To be more precise, 
+    //an 401 unauthorized status with a response message of ‘No token provided’.
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    
+    //If the token exists, the jwt.verify() method will be called.
+    // This method decodes the token making it possible to view the original payload.
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+      
+      //if there are no errors
+    //   send back the decoded value as the response.
+      res.status(200).send(decoded);
+    });
+  });
   
